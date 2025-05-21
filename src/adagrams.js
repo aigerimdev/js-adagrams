b275ac5bbe0aa7f5ff040f7580fd2394bee308e2
Aigerim Kalygulova <kalygulovas@gmail.com>
2025-05-21 14:48:34 -0700

    Helper function: Build a sack of all letters based on frequency

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,3 +1,17 @@
+const letterPool = {
+    'A': 9, 'B': 2, 'C': 2, 'D': 4, 'E': 12, 'F': 2, 'G': 3, 'H': 2, 'I': 9, 
+    'J': 1, 'K': 1, 'L': 4, 'M': 2, 'N': 6, 'O': 8, 'P': 2, 'Q': 1, 'R': 6, 
+    'S': 4, 'T': 6, 'U': 4, 'V': 2, 'W': 2, 'X': 1, 'Y': 2, 'Z': 1
+}
+//helper function 
+const buildSackOfLetters = () =>{
+  let letters = [];
+  Object.entries(letterPool).forEach(([letter, count])=> {
+    letters.push(...Array(count).fill(letter));
+  });
+  return letters;
+};
+
 export const drawLetters = () => {
   // Implement this method for wave 1
 };
